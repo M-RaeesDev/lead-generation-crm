d9903be70e99cd86c37b73bea5b21f889a01ff41
M-RaeesDev <raeesawan361@gmail.com>
2025-08-12 15:38:19 +0500

    Add NextAuthjs continue with Google Provider

---
+++ client/app/page.jsx
@@ -0,0 +1,9 @@
+import Userinfo from "@/components/Userinfo";
+
+export default function Home() {
+  return (
+    <div className="grid place-items-center h-screen mt-24">
+      <Userinfo/>
+    </div>
+  );
+}
